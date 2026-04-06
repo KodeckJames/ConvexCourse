be8b0f6156c45958aea15c97f827b4320fdd3622
JJ <153640426+KodeckJames@users.noreply.github.com>
2026-04-06 17:51:33 +0300

    Chore: Configured the form logic

--- app/Home/page.tsx
+++ app/Home/page.tsx
@@ -1,9 +1,24 @@
+'use client';
+
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
+
 export default function HomePage() {
+
+  const createNote=useMutation(api.notes.createNote)
+
   return (
     <div className=" min-h-dvh flex justify-center flex-col items-center">
       <h1 className=" text-4xl text-blue-500 font-extrabold">HomePage</h1>
-      <form action="">
-        <input type="text" />
+      <form onSubmit={(e) => {
+        e.preventDefault();
+        const form = e.target as HTMLFormElement;
+        const formData = new FormData(form);
+        const note = formData.get("note") as string;
+        void createNote({ note });
+        form.reset();
+      }}>
+        <input type="text" className=" bg-white border border-white" />
         <button>Submit</button>
       </form>
     </div>
