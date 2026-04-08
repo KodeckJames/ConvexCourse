bddf301a4a8ae432d624f94434702e70b2c193cd
JJ <153640426+KodeckJames@users.noreply.github.com>
2026-04-08 16:27:02 +0300

    File Storage

--- convex/notes.ts
+++ convex/notes.ts
@@ -1,13 +1,17 @@
 import { convexToJson, v } from "convex/values";
-import { internalMutation, mutation, query } from "./_generated/server";
+import {
+  internalAction,
+  internalMutation,
+  mutation,
+  query,
+} from "./_generated/server";
 import { getAuthUserId } from "@convex-dev/auth/server";
 
 import { RateLimiter, MINUTE, HOUR } from "@convex-dev/rate-limiter";
-import { components } from "./_generated/api";
+import { components, internal } from "./_generated/api";
 
-const rateLimiter = new RateLimiter(components.rateLimiter , {
+const rateLimiter = new RateLimiter(components.rateLimiter, {
   createNote: { kind: "fixed window", rate: 1, period: MINUTE },
-  
 });
 
 export const createNote = mutation({
@@ -19,12 +23,15 @@ export const createNote = mutation({
     const identity = await ctx.auth.getUserIdentity();
     if (!identity) throw new Error("Unauthorized");
     const userId = identity.subject;
-    await rateLimiter.limit(ctx, "createNote", {key: identity.subject, throws:true});
-    // if (!userId) {
-    //   throw new Error("Unauthorized");
-    // }
+    await rateLimiter.limit(ctx, "createNote", {
+      key: identity.subject,
+      throws: true,
+    });
     await ctx.db.insert("notesTable", {
       userId,
+      note: args.note,
+    });
+    await ctx.scheduler.runAfter(0, internal.notes.createNoteFile, {
       note: args.note,
     });
   },
@@ -34,9 +41,9 @@ export const deleteNotes = internalMutation({
   args: {},
   handler: async (ctx, args) => {
     const notes = await ctx.db.query("notesTable").collect();
-    await Promise.all(notes.map((note)=> ctx.db.delete(note._id)))
-  }
-})
+    await Promise.all(notes.map((note) => ctx.db.delete(note._id)));
+  },
+});
 
 export const getNotes = query({
   args: {},
@@ -45,5 +52,15 @@ export const getNotes = query({
     if (!identity) throw new Error("Unauthorized");
 
     return await ctx.db.query("notesTable").collect();
+  },
+});
+
+// File handling
+export const createNoteFile = internalAction({
+  args: {
+    note: v.string(),
+  },
+  handler: async (ctx, args) => {
+    await ctx.storage.store(new Blob([args.note]));
   },
 });
